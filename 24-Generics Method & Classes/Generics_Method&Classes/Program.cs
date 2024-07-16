76ef44ca50fda616b100ed804265b0591b520fec
Junaid Khalid <57348907+Junaid20001@users.noreply.github.com>
2024-07-16 12:15:26 +0500

    Add files via upload

---
+++ 24-Generics Method & Classes/Generics_Method&Classes/Program.cs
@@ -0,0 +1,119 @@
+﻿using Generics_Method_Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Generics_Method_Classes
+{
+    /*
+    // Generics Method Example
+    class Example
+    {
+        // Method to display any type of array
+        public static void showArray<T>(T[] arr)
+        {
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        // Generic method to check equality of two values
+        public static bool check<T>(T a, T b)
+        {
+            return a.Equals(b);
+        }
+    }
+
+    // Main program
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Example arrays
+            int[] Numbers = new int[] { 11, 22, 33 };
+            string[] Names = { "Junaid", "Khalid", "Mehmood" };
+            double[] Points = { 5.7, 7.8, 2.5 };
+
+            // Using Example class methods
+            Example.showArray(Numbers); // Display integers
+            Example.showArray(Names);   // Display strings
+            Example.showArray(Points);  // Display doubles
+
+            // Checking equality using generics method
+            Console.WriteLine(Example.check(2, 5)); // Should return False
+            Console.WriteLine(Example.check(5, 5)); // Should return True
+
+            Console.ReadLine();
+        }
+    }
+    */
+
+
+    //Generics classes
+    /*
+    class Example <T>
+    {
+        T box;
+        public T Box
+        {
+            set
+            {
+                this.box = value;  ;
+            }
+            get
+            {
+              return this.box  ;
+            }
+        
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Example<int> e = new Example<int>();
+            Example<string> e1 = new Example<string>();
+            e.Box = 30;
+            e1.Box = "JK";
+            Console.WriteLine(e.Box);
+            Console.WriteLine(e1.Box);
+            Console.ReadLine();
+        }
+    }
+    */
+
+    //OR
+
+    /*
+    class Example<T>
+    {
+        T box;
+        public Example (T b)
+        {
+            this.box = b;
+        }
+        public T getBox()
+        {
+            return this.box;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Example<int> e = new Example<int>(20);
+            Example<string> e1 = new Example<string>("Junaid");
+            Console.WriteLine(e.getBox());
+            Console.WriteLine(e1.getBox());
+            Console.ReadLine();
+        }
+    }
+    */
+}
---
+++ 25-Hash_Table/Hash_Table/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hash_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable ht  = new Hashtable();
+            ht.Add("Id", 2256);
+            ht.Add("Name", "Junaid");
+            ht.Add("Age", 23);
+            ht.Add("Designation", "Manager");
+            ht.Add("IsMarried", false);
+
+            //Console.WriteLine(ht["Name"]);    //Print for single value
+
+            /*
+            foreach(object key in ht.Keys)     //print all value but unorder
+            {
+                Console.WriteLine(key + " : " + ht[key]);
+            }
+            */
+
+            /*
+            ht.Remove("Age");    //remove one key
+            foreach (object key in ht.Keys)
+            {
+                Console.WriteLine(key + " : " + ht[key]);
+            }
+            */
+
+            /*
+            ht.Clear();    //remove all key after but past print key is placed
+            foreach (object key in ht.Keys)
+            {
+                Console.WriteLine(key + " : " + ht[key]);
+            }
+            */
+
+            /*
+            //Contain is for check weather the key is place or not in code.
+            Console.WriteLine(ht.Contains("FName"));   //False becuase of Fname
+            */
+
+            /*
+            //Contain value is for check weather the value is place or not in code.
+            Console.WriteLine(ht.ContainsValue("Junaid"));  //True because name value is correct
+            */
+            Console.ReadLine();
+        }
+    }
+}
